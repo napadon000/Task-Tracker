51eca60be63ecababd7cb948a7fb3dff9ff62ce6
Napadon <napadonnantasuk@gmail.com>
2025-05-25 22:06:03 +0700

    add cors

--- Backend/main.go
+++ Backend/main.go
@@ -7,11 +7,18 @@ import (
 
 	// dotenv "github.com/dsh2dsh/expx-dotenv"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	configs.ConnectDatabase()
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:3000",
+		AllowMethods: "GET, POST, PUT, PATCH, DELETE, OPTIONS",
+		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
+	}))
 
 	userGroup := app.Group("/api")
 	routes.UserRoute(userGroup)
